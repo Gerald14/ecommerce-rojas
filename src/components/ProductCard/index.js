119e4cf07c09817d1de7e12cebb8af1f8382e6b4
betsirm <betsabetgeraldrm@gmail.com>
2022-06-14 22:14:34 -0500

    add styles to card

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -2,30 +2,39 @@ import Card from "@mui/material/Card"
 import CardMedia from "@mui/material/CardMedia"
 import CardContent from "@mui/material/CardContent"
 import Typography from "@mui/material/Typography"
-import { CardActions } from "@mui/material";
-import ItemCount from "../ItemCount";
+import { CardActions, IconButton } from "@mui/material";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import { Box } from "@mui/system";
 const mangasImg = require.context('../../assets/images/mangas', true);
 
 
 const ProductCard = ({product}) => {
   return (
-    <Card sx={{ maxWidth: 335, '.MuiCardMedia-root':{height:'auto'}}}>
+    <Card sx={{ maxWidth: 335, '.MuiCardMedia-root':{height:'21rem',objectFit:'cover'}}} className="product">
       <CardMedia
         component="img"
         height="140"
         image={ mangasImg(`./${product.img}`) }
         alt={product.name}
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" sx={{ fontSize: 16 }} component="div">
+      <CardContent sx={{height:'inherit'}}>
+        <Typography gutterBottom variant="h5" sx={{ fontSize: 16 }} component="div" textAlign='center'>
           {product.name}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary" textAlign='center'>
           {product.price}
         </Typography>
       </CardContent>
-      <CardActions>
-        <ItemCount stock={product.stock}/>
+      <CardActions sx={{display:{xs:'none',sm:'block'}}}>
+        <Box component="div" sx={{display:'flex', gap:'.3rem',alignItems:'center'}}>
+          <IconButton variant="contained" aria-label="btn-view" sx={{backgroundColor:'#272829',color:"white",'&:hover':{backgroundColor:'#272829'}  }}>
+            <RemoveRedEyeOutlinedIcon/>
+          </IconButton>
+          <IconButton variant="contained" aria-label="btn-cart" sx={{backgroundColor:'#272829',color:"white",'&:hover':{backgroundColor:'#272829'} }}>
+            <ShoppingCartOutlinedIcon/>
+          </IconButton>
+        </Box>
       </CardActions>
     </Card>
   )
