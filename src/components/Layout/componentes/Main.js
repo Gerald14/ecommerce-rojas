1dbcdeadc913e27b49a0cb0dcbeb487918518037
betsirm <betsabetgeraldrm@gmail.com>
2022-06-16 22:50:56 -0500

    add new navbar responsive without menu mobile

--- src/components/Layout/componentes/Main.js
+++ src/components/Layout/componentes/Main.js
@@ -1,10 +1,11 @@
 
-import styled from '@emotion/styled'
+import { styled } from '@mui/material/styles'
 
 const Main = styled('main')(({theme}) => ({
     flexGrow:1,
     width:'100%',
-    marginLeft:'0px'
+    marginLeft:'0px',
+    marginTop: theme.mixins.toolbar.minHeight
 }))
 
 export default Main
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,53 +3,82 @@ import Avatar from '../Avatar'
 import User from '../../assets/images/User-icon.png'
 import { MenuData } from './components/MenuData'
 import CartWidget from '../CartWidget'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { AppBar, Box, Container, IconButton, Toolbar } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
+  const navigate = useNavigate();
 
   let activeStyle = {
     color: "red",
   };
+  const handleClick = () => {
+    navigate('/')
+  }
 
   return (
-    <header className="navbar">
-      <div className="container">
+    <AppBar className="navbar">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
 
-        <div className="navbar-brand">
-          <img src={Logo} alt="Logo" />
-          <div className="navbar-brand-text">
-            <span>Manga</span>
-            <span>Store</span>
-          </div>
-        </div>
+          <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={()=>null}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
 
-        <nav className="menu-main">
-          <ul>
-            {MenuData.map((item,index) =>
-              (<li key={index}>
-                <NavLink 
-                  to={item.path}
-                  style={({ isActive }) =>
-                    isActive ? activeStyle : undefined
-                  }
-                >
-                    {item.title}
-                  </NavLink>
-                </li>)
-            )}
-          </ul>
-        </nav>
-        <div className='menu-secondary'>
-            <div className="navbar-search">
-              <img src="https://img.icons8.com/ios-glyphs/26/ffffff/search--v1.png"alt='search'/>
+          <Box 
+            component={'div'} 
+            className="navbar-brand" 
+            sx={{flexGrow:{xs:1,md:0}}}
+            onClick={handleClick}>
+            <img src={Logo} alt="Logo" />
+            <div className="navbar-brand-text">
+              <span>Manga</span>
+              <span>Store</span>
             </div>
-            <div className="navbar-account">
-              <Avatar url={User}/>
-            </div>
-            <CartWidget/>
-        </div>
-      </div>
-    </header>
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, marginLeft:'2rem' }}>
+            <nav className="menu-main" >
+              <ul>
+                {MenuData.map((item,index) =>
+                  (<li key={index}>
+                    <NavLink 
+                      to={item.path}
+                      style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                      }
+                    >
+                        {item.title}
+                      </NavLink>
+                    </li>)
+                )}
+              </ul>
+            </nav>
+          </Box>
+          
+          <Box component={'div'} className='menu-secondary' sx={{ flexGrow: 0 }}>
+              <div className="navbar-search">
+                <img src="https://img.icons8.com/ios-glyphs/26/ffffff/search--v1.png"alt='search'/>
+              </div>
+              <div className="navbar-account">
+                <Avatar url={User}/>
+              </div>
+              <CartWidget/>
+          </Box>
+        </Toolbar>
+        
+      </Container>
+    </AppBar>
   )
 }
 
